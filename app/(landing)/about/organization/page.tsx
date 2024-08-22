0979598e5f1f1cbec24ec5812f7edaeb69f545e7
Joshua Inyang <joshuainyang255@gmail.com>
2024-08-22 08:45:46 +0100

    assets updates

--- app/(landing)/about/organization/page.tsx
+++ app/(landing)/about/organization/page.tsx
@@ -86,9 +86,9 @@ const OrganizationPage = () => {
             <div className="relative flex-shrink-0 size-80 lg:size-[80vh] ">
               <Image
                 fill
-                src="/6.png"
-                alt="truth image"
-                className=" object-cover object-top rounded-full"
+                src="https://images.unsplash.com/photo-1473081556163-2a17de81fc97?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bGVhZnxlbnwwfHwwfHx8MA%3D%3D"
+                alt="leaf tree image"
+                className=" object-cover object-top rounded-full "
               />
             </div>
 
